db0df5168f8fe19fd0d74aef76f5eaf2b055baed
Ho-Huan Chiang <hc57@rice.edu>
2021-09-26 17:06:58 -0500

    Add project files.

---
+++ ConvertJob/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using PhilipsProject.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ConvertJob.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ConvertProcess process = new ConvertProcess();
+            process.Start();
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ ConvertJob/Models/ConvertProcess.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Web;
+
+namespace PhilipsProject.Models
+{
+    public class ConvertProcess
+    {
+        private Process process;
+
+        public void Start()
+        {
+            process = new Process();
+            process.StartInfo.FileName = @"C:\philips\isyntaxConverter.exe";
+            process.StartInfo.Arguments = @"convert C:\philips\1.isyntax 0 0 0";
+            process.StartInfo.UseShellExecute = false;
+            process.StartInfo.CreateNoWindow = false;
+            process.StartInfo.RedirectStandardOutput = true;
+            process.StartInfo.RedirectStandardError = true;
+            //* Set your output and error (asynchronous) handlers
+            process.OutputDataReceived += (sendingProcess, outLine) => OutputConvertHandler(sendingProcess, outLine);
+            process.ErrorDataReceived += (sendingProcess, outLine) => ErrorConvertHandler(sendingProcess, outLine);
+
+            process.Start();
+            process.BeginOutputReadLine();
+            process.BeginErrorReadLine();
+        }
+
+        private void OutputConvertHandler(object sendingProcess, DataReceivedEventArgs outLine)
+        {
+            if (!String.IsNullOrEmpty(outLine.Data))
+            {
+                Debug.WriteLine(outLine.Data);
+                if (outLine.Data.Trim() == "TIFF Successfully Generated")
+                {
+                    Debug.WriteLine("Success");
+                }
+            }
+
+
+        }
+        private void ErrorConvertHandler(object sendingProcess, DataReceivedEventArgs outLine)
+        {
+            if (!String.IsNullOrEmpty(outLine.Data))
+            {
+                Debug.WriteLine("error: " + outLine.Data);
+            }
+        }
+    }
+}
